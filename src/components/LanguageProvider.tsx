5a97a6b0e2fb97285fcc62a7d09acb26ccf1286e
= <86160611+ManuCiber@users.noreply.github.com>
2026-04-19 07:39:11 -0400

    File Created: UseLanguage

---
+++ src/components/LanguageProvider.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import * as React from "react"
+import { Language, translations } from "./translations"
+
+interface LanguageContextType {
+  language: Language
+  setLanguage: (lang: Language) => void
+  translation: typeof translations.en
+}
+
+const LanguageContext = React.createContext<LanguageContextType | undefined>(undefined)
+
+export function LanguageProvider({ children }: { children: React.ReactNode }) {
+  const [language, setLanguage] = React.useState<Language>("en")
+
+  const translation = translations[language]
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage, translation }}>
+      {children}
+    </LanguageContext.Provider>
+  )
+}
+
+export function useLanguage() {
+  const context = React.useContext(LanguageContext)
+  if (context === undefined) {
+    throw new Error("useLanguage must be used within a LanguageProvider")
+  }
+  return context
+}
